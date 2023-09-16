559f9b32fe6d3f6831d081b3a9fca7ae12cff323
ajayiseyi2304 <ajayiseyi2304@gmail.com>
2023-09-16 10:54:56 -0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/header/Header";
-import Home from "./pages/home/home";
-import MovieList from "./components/movieList/movieList";
-import Movie from "./pages/movieDetail/movie";
+import Body from "./Body";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+// import AppRouter from "./AppRouter";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route index element={<Home />}></Route>
-          <Route path="movie/:id" element={<Movie />}></Route>
-          <Route path="movies/:type" element={<MovieList />}></Route>
-          <Route path="/*" element={<h1>Error Page</h1>}></Route>
-        </Routes>
-      </Router>
+    <div className="font-body bg-primary-100">
+      <React.StrictMode>
+        <BrowserRouter>
+          <Body />
+          {/* <AppRouter /> Use your router component */}
+        </BrowserRouter>
+      </React.StrictMode>
     </div>
   );
 }
